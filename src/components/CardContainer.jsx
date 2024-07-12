15cbb990fc1bfe30deea1744cf172218d3cf6a94
erkansevim <erkansevim.essgmail.com>
2024-07-12 11:31:59 +0200

    created search bar

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {data} from "../helper/data"
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row, Form} from "react-bootstrap";
 import Player from './Player';
 
 const CardContainer = () => {
   return (
     <>
+     <Form.Control className='w-50 m-auto' type="search" placeholder="Search Player..." readOnly />
         <Container className='kart-container rounded-4 my-4 p-3'>
             <Row className='g-3 justify-content-center'>
                 {data.map((player)=>(
