393238fabbc01423c722c5089ad019f7d1831e47
erkansevim <erkansevim.essgmail.com>
2024-07-12 11:24:53 +0200

    created cards

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {data} from "../helper/data"
+import {Col, Container, Row} from "react-bootstrap";
+import Player from './Player';
 
 const CardContainer = () => {
   return (
-    <div>CardContainer</div>
+    <>
+        <Container className='kart-container rounded-4 my-4 p-3'>
+            <Row className='g-3 justify-content-center'>
+                {data.map((player)=>(
+                   
+                    <Col md={6} lg={4} xl={3} key={player.name}>
+                        <Player {...player}/>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+
+
+
+    </>
   )
 }
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { Card } from 'react-bootstrap'
+
+const Player = ({name, img, statistics}) => {
+const [showResim, setShow]=useState(true)
+
+  return (
+    <Card className='rounded-2 m-auto player-kart 'role="button" onClick={()=>setShow(!showResim)}>
+        
+{showResim ? (<Card.Img className="player-resim" variant="top" src={img}/>):(
+    <ul className="m-auto">
+        {statistics.map((item, index)=>(
+            <li key={index} className="h5 list-unstyled">
+            🏀{item}</li>
+        ))}
+    </ul>
+)}
+
+        <Card.Footer>
+            {name}
+        </Card.Footer>
+    </Card>
+  )
+}
+
+export default Player
