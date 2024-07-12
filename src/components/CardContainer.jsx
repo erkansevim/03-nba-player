701a5727270ba73e783211634d04e1872e4955c5
erkansevim <erkansevim.essgmail.com>
2024-07-12 12:03:19 +0200

    hooks renamed

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -5,7 +5,7 @@ import {Col, Container, Row, Form} from "react-bootstrap";
 import Player from './Player';
 
 const CardContainer = () => {
-    const [arama,setArama]=useState("")
+    const [ask,setArama]=useState("")
     //1.yol
     // let filtrelenmisdata;
     // filtrelenmisdata=data.filter((a)=>a.name.includes(arama))
@@ -15,7 +15,7 @@ const CardContainer = () => {
      <Form.Control className="w-50 m-auto" type="search" placeholder="Search Player..." onChange={(e)=>setArama(e.target.value)}/>
         <Container className='kart-container rounded-4 my-4 p-3'>
             <Row className='g-3 justify-content-center'>
-                {data.filter((abc)=>abc.name.toLowerCase().includes(arama.toLowerCase())).map((player)=>(
+                {data.filter((abc)=>abc.name.toLowerCase().includes(ask.toLowerCase())).map((player)=>(
                    
                     <Col md={6} lg={4} xl={3} key={player.name}>
                         <Player {...player}/>
