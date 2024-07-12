8cc825c05fda6b4b3daa658cd103b545a5ff1291
erkansevim <erkansevim.essgmail.com>
2024-07-12 11:56:47 +0200

    search bar is ready

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 import {data} from "../helper/data"
 import {Col, Container, Row, Form} from "react-bootstrap";
 import Player from './Player';
 
 const CardContainer = () => {
+    const [arama,setArama]=useState("")
+    //1.yol
+    // let filtrelenmisdata;
+    // filtrelenmisdata=data.filter((a)=>a.name.includes(arama))
+
   return (
     <>
-     <Form.Control className='w-50 m-auto' type="search" placeholder="Search Player..." readOnly />
+     <Form.Control className="w-50 m-auto" type="search" placeholder="Search Player..." onChange={(e)=>setArama(e.target.value)}/>
         <Container className='kart-container rounded-4 my-4 p-3'>
             <Row className='g-3 justify-content-center'>
-                {data.map((player)=>(
+                {data.filter((abc)=>abc.name.includes(arama)).map((player)=>(
                    
                     <Col md={6} lg={4} xl={3} key={player.name}>
                         <Player {...player}/>
